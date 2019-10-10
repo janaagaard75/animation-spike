a41f81fe965f6b259a22ab39672e923f261e090c
Jan Aagaard <jan@aagaard.net>
2019-10-10 22:15:48 +0200

    Use the specific ViewStyle style type

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Text, View, ViewStyle } from "react-native"
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center"
-  }
-})
+const containerStyles: ViewStyle = {
+  alignItems: "center",
+  backgroundColor: "#fff",
+  flex: 1,
+  justifyContent: "center"
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={containerStyles}>
+      <Text>Open up App.tsx to start working on your app.</Text>
     </View>
   )
 }

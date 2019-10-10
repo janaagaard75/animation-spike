e3641572eeac0ce1b8c0ade4045661abe299276a
Jan Aagaard <jan@aagaard.net>
2019-10-10 22:12:50 +0200

    Sort style properties

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View } from "react-native"
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
     justifyContent: "center"
   }
 })

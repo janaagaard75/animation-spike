979adbac4424568e370c7e9ed58095cfd2f72496
Jan Aagaard <jan@aagaard.net>
2019-10-13 22:12:12 +0200

    Position based on state

--- src/PositionedSquare.tsx
+++ src/PositionedSquare.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { View } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { Square } from "./Square"
 
 enum Position {
@@ -10,23 +10,60 @@ enum Position {
 }
 
 interface State {
-  SquarePosition: Position
+  squarePosition: Position
 }
 
 export class PositionedSquare extends Component<{}, State> {
+  public constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      squarePosition: Position.BottomRight
+    }
+  }
+
   public render() {
     return (
       <View
-        style={{
-          alignItems: "flex-end",
-          display: "flex",
-          height: 300,
-          justifyContent: "flex-end",
-          width: 300
-        }}
+        style={[
+          {
+            display: "flex",
+            height: 300,
+            width: 300
+          },
+          PositionedSquare.getPositionStyle(this.state.squarePosition)
+        ]}
       >
         <Square />
       </View>
     )
+  }
+
+  private static getPositionStyle(position: Position): ViewStyle {
+    switch (position) {
+      case Position.BottomLeft:
+        return {
+          alignItems: "flex-end",
+          justifyContent: "flex-start"
+        }
+
+      case Position.BottomRight:
+        return {
+          alignItems: "flex-end",
+          justifyContent: "flex-end"
+        }
+
+      case Position.TopLeft:
+        return {
+          alignItems: "flex-start",
+          justifyContent: "flex-start"
+        }
+
+      case Position.TopRight:
+        return {
+          alignItems: "flex-start",
+          justifyContent: "flex-end"
+        }
+    }
   }
 }

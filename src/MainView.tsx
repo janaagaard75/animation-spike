dcd090f041f7edb785f509195e952632fb7f1525
Jan Aagaard <jan@aagaard.net>
2019-10-14 21:35:14 +0200

    Make MainView a function

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -1,63 +1,50 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Button, View } from "react-native"
 import { Position } from "./Position"
 import { PositionedSquare } from "./PositionedSquare"
 
-interface State {
-  squarePosition: Position
+const getNewRandomPosition = (currentPosition: Position): Position => {
+  for (;;) {
+    const newPosition = getRandomPosition()
+    if (newPosition !== currentPosition) {
+      return newPosition
+    }
+  }
 }
 
-export class MainView extends Component<{}, State> {
-  public constructor(props: {}) {
-    super(props)
+const getRandomPosition = (): Position => {
+  return getRandomInteger(0, 3)
+}
 
-    this.state = {
-      squarePosition: Position.TopLeft
-    }
-  }
+const getRandomInteger = (minimum: number, maximum: number): number => {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+}
 
-  public render() {
-    return (
+export const MainView = () => {
+  const [squarePosition, setSquarePosition] = useState(Position.TopLeft)
+
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: "center"
+      }}
+    >
+      <PositionedSquare destination={squarePosition} />
       <View
         style={{
-          alignItems: "center",
-          backgroundColor: "#fff",
-          flex: 1,
-          justifyContent: "center"
+          marginTop: 40
         }}
       >
-        <PositionedSquare destination={this.state.squarePosition} />
-        <View
-          style={{
-            marginTop: 40
-          }}
-        >
-          <Button onPress={() => this.moveSquare()} title="Move square" />
-        </View>
+        <Button
+          onPress={() =>
+            setSquarePosition(getNewRandomPosition(squarePosition))
+          }
+          title="Move square"
+        />
       </View>
-    )
-  }
-
-  private moveSquare() {
-    this.setState({
-      squarePosition: MainView.getNewRandomPosition(this.state.squarePosition)
-    })
-  }
-
-  private static getNewRandomPosition(currentPosition: Position): Position {
-    for (;;) {
-      const newPosition = this.getRandomPosition()
-      if (newPosition !== currentPosition) {
-        return newPosition
-      }
-    }
-  }
-
-  private static getRandomPosition(): Position {
-    return MainView.getRandomInteger(0, 3)
-  }
-
-  private static getRandomInteger(minimum: number, maximum: number): number {
-    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
-  }
+    </View>
+  )
 }

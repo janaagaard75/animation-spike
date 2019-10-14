85a09394f68906e41fb877efec1cdf918b77c096
Jan Aagaard <jan@aagaard.net>
2019-10-14 21:27:05 +0200

    Make PositionedSquare a function

--- src/PositionedSquare.tsx
+++ src/PositionedSquare.tsx
@@ -1,55 +1,53 @@
-import React, { Component } from "react"
+import React from "react"
 import { View, ViewStyle } from "react-native"
 import { Position } from "./Position"
 import { Square } from "./Square"
+
+const getPositionStyle = (position: Position): ViewStyle => {
+  switch (position) {
+    case Position.BottomLeft:
+      return {
+        alignItems: "flex-start",
+        justifyContent: "flex-end"
+      }
+
+    case Position.BottomRight:
+      return {
+        alignItems: "flex-end",
+        justifyContent: "flex-end"
+      }
+
+    case Position.TopLeft:
+      return {
+        alignItems: "flex-start",
+        justifyContent: "flex-start"
+      }
+
+    case Position.TopRight:
+      return {
+        alignItems: "flex-end",
+        justifyContent: "flex-start"
+      }
+  }
+}
 
 interface Props {
   destination: Position
 }
 
-export class PositionedSquare extends Component<Props> {
-  public render() {
-    return (
-      <View
-        style={[
-          {
-            display: "flex",
-            height: 300,
-            width: 300
-          },
-          PositionedSquare.getPositionStyle(this.props.destination)
-        ]}
-      >
-        <Square />
-      </View>
-    )
-  }
-
-  private static getPositionStyle(position: Position): ViewStyle {
-    switch (position) {
-      case Position.BottomLeft:
-        return {
-          alignItems: "flex-start",
-          justifyContent: "flex-end"
-        }
-
-      case Position.BottomRight:
-        return {
-          alignItems: "flex-end",
-          justifyContent: "flex-end"
-        }
-
-      case Position.TopLeft:
-        return {
-          alignItems: "flex-start",
-          justifyContent: "flex-start"
-        }
-
-      case Position.TopRight:
-        return {
-          alignItems: "flex-end",
-          justifyContent: "flex-start"
-        }
-    }
-  }
+export const PositionedSquare = (props: Props) => {
+  return (
+    <View
+      style={[
+        {
+          display: "flex",
+          height: 300,
+          width: 300
+        },
+        getPositionStyle(props.destination)
+      ]}
+    >
+      <Square />
+    </View>
+  )
 }

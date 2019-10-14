f751f2e7f69e6de4e5809d4801ebd51e1c787060
Jan Aagaard <jan@aagaard.net>
2019-10-14 22:24:56 +0200

    Show position destination

--- src/MainView.tsx
+++ src/MainView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Button, View } from "react-native"
+import { Button, Text, View } from "react-native"
 import { Position } from "./Position"
 import { PositionedSquare } from "./PositionedSquare"
 
@@ -20,6 +20,19 @@ const getNewRandomPosition = (currentPosition: Position): Position => {
   }
 }
 
+const getPositionName = (position: Position) => {
+  switch (position) {
+    case Position.BottomLeft:
+      return "Bottom left"
+    case Position.BottomRight:
+      return "Bottom right"
+    case Position.TopLeft:
+      return "Top left"
+    case Position.TopRight:
+      return "Top right"
+  }
+}
+
 export const MainView = () => {
   const [squarePosition, setSquarePosition] = useState(Position.TopLeft)
 
@@ -33,9 +46,16 @@ export const MainView = () => {
       }}
     >
       <PositionedSquare destination={squarePosition} />
+      <Text
+        style={{
+          marginTop: 20
+        }}
+      >
+        Destination: {getPositionName(squarePosition)}
+      </Text>
       <View
         style={{
-          marginTop: 40
+          marginTop: 20
         }}
       >
         <Button

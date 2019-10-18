e75cf9a2b7ff406d96bbf3f0d2cf9cf0039affef
Jan Aagaard <jan@aagaard.net>
2019-10-18 16:09:22 +0200

    Add dragging (buggy, though)

--- src/PositionedSquareClass.tsx
+++ src/PositionedSquareClass.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Animated } from "react-native"
+import { Animated, PanResponder, PanResponderInstance } from "react-native"
 import { fieldSize } from "./constants"
 import { Coordinates } from "./Coordinates"
 import { Square } from "./Square"
@@ -27,7 +27,23 @@ export class PositionedSquareClass extends Component<Props, State> {
         this.setState({ animating: false })
       }
     })
+
+    // TODO: This only works when destination is (0, 0). Should dragging be moved to a sub-component?
+    this.panResponder = PanResponder.create({
+      onPanResponderMove: (e, gestureState) => {
+        Animated.event([
+          null,
+          {
+            dx: this.state.animatedPosition.x,
+            dy: this.state.animatedPosition.y
+          }
+        ])(e, gestureState)
+      },
+      onStartShouldSetPanResponder: (_e, _gestureState) => true
+    })
   }
+
+  private panResponder: PanResponderInstance
 
   public componentDidUpdate(prevProps: Props, _prevState: State) {
     if (prevProps.destination === this.props.destination) {
@@ -53,6 +69,7 @@ export class PositionedSquareClass extends Component<Props, State> {
             width: fieldSize
           }
         ]}
+        {...this.panResponder.panHandlers}
       >
         <Square animating={this.state.animating} />
       </Animated.View>
